ae719bc3ffd18481cec905f61a4e1d053ff417c3
Harrison Affel <harrisonaffel@gmail.com>
2023-07-27 12:03:49 -0400

    rm local tls code

--- go/main.go
+++ go/main.go
@@ -20,15 +20,6 @@ func main() {
 		Credentials: controller,
 	}
 
-	err = pkg.CreateDir(os.Getenv("ACTIVE_DIRECTORY"), "")
-	if err != nil {
-		panic(fmt.Sprintf("failed to create dynamic directory : %v", err))
-	}
-	err = pkg.GenCerts(os.Getenv("ACTIVE_DIRECTORY"))
-	if err != nil {
-		panic(fmt.Errorf("%v : could not generate certificates", err))
-	}
-
 	errChan := make(chan error)
 	port := server.StartServer(errChan, os.Getenv("ACTIVE_DIRECTORY"))
 	err = pkg.CreateDir(os.Getenv("ACTIVE_DIRECTORY"), port)
